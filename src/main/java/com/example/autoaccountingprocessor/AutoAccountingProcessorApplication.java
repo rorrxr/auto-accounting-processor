c98355f4e850117afedd4a366342cd61f18547c0
rorrxr <rorrxr@naver.com>
2025-08-07 21:57:00 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/autoaccountingprocessor/AutoAccountingProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoaccountingprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAccountingProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoAccountingProcessorApplication.class, args);
+    }
+
+}
